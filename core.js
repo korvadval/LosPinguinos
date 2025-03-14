7c527d8694bd9d12d5b5f21090365427d98c93f3
vkorneyuk <kor.vad.val@yandex.ru>
2025-03-14 20:11:16 +0600

    check connect tg app

--- core.js
+++ core.js
@@ -1,5 +1,6 @@
 const PINGUIN_IMAGE_ID = 'pinguin_image'
 const PINGUIN_AUDIO_ID = 'pinguin_audio'
+const LOG_INFO_ID = 'lod_info'
 const PINGUINS = [
     {id: 'kawazaki', image_url: 'assets/images/Kawazaki_img.png', audio_url: 'assets/sounds/Kawazaki_sound.mp3'},
     {id: 'cago', image_url: 'assets/images/Cago_img.png', audio_url: 'assets/sounds/Cago_sound.mp3'},
@@ -15,4 +16,17 @@ function render(pinguin) {
     audio_el.src = pinguin.audio_url
 }
 
+function log(info) {
+    const log_el = document.getElementById(LOG_INFO_ID)
+    log_el.innerText = info
+}
+
 // render(PINGUINS.find(pinguin => pinguin.id === 'estriper'))
+document.addEventListener('DOMContentLoaded', () => {
+    const tg = window.Telegram?.WebApp
+
+    if (tg) {
+        tg.expand()
+        log(`User: ${tg.initDataUnsafe.user}`)
+    }
+})
